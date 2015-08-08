ba19daacfde5f5210b6df207bd5fdb76cddc1c07
Dave Meikle <dmeikle@apache.org>
2015-08-08 13:23:04 +0100

    Updated to take version and made executable

--- release-notes/release_notes_parse.rb
+++ release-notes/release_notes_parse.rb
@@ -1,9 +1,16 @@
+#!/usr/bin/env ruby
+
 require 'rubygems'
 require 'nokogiri' 
 require 'open-uri'
 
+if ARGV.empty?
+  puts "Usage: release_notes_parse.rb tikaVersion"
+  exit 1
+end
+
 # Get latest Tika release report (i.e. full dump of filter)
-doc = Nokogiri::HTML(open('https://s.apache.org/TikaLatestVersionReport'))
+doc = Nokogiri::HTML(open("https://issues.apache.org/jira/sr/jira.issueviews:searchrequest-printable/temp/SearchRequest.html?jqlQuery=project+%3D+TIKA+AND+fixVersion+%3D+#{ARGV[0]}&tempMax=100"))
 
 names = Array.new
 doc.xpath('//a[@class="user-hover"]').each do | user |
